28cc06893d6814f312fe7562c10d1cc65f3d83b3
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-11-16 17:19:13 +0700

    #1 Commit"

---
+++ ListViewComposite/src/id/web/yht/listviewcomposite/DataList.java
@@ -0,0 +1,11 @@
+package id.web.yht.listviewcomposite;
+
+public class DataList {
+	public String judul;
+	public String keterangan;
+	
+	public DataList(String judul, String keterangan) {
+		this.judul = judul;
+		this.keterangan = keterangan;
+	}
+}
